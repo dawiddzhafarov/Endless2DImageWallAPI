2606fda565a9af8af95c0f5ca0a10056006057c6
Azsael <56028507+Azsaell@users.noreply.github.com>
2022-11-10 18:35:43 +0100

    nic nie działa ale chyba będzie ładniej xd

--- Endless2DImageWall/app/src/main/java/com/example/endless2dimagewall/MainActivity.java
+++ Endless2DImageWall/app/src/main/java/com/example/endless2dimagewall/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
@@ -19,6 +20,8 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.io.IOUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -37,13 +40,17 @@ import java.sql.SQLOutput;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.lang.Object;
+import java.util.concurrent.CompletableFuture;
+
 import org.json.*;
 
+import reactor.core.publisher.Mono;
 
 
 public class MainActivity extends AppCompatActivity{
 
     float dx=0,dy=0,x=0,y=0;
+    URL url;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -52,7 +59,16 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.main);
 
-        new JsonTask().execute("http://10.0.2.2:8080/image");
+        //new JsonTask().execute("http://10.0.2.2:8080/image");
+        try {
+            url = new URL("http://10.0.2.2:8080/images");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            url = null;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            CompletableFuture.supplyAsync(() -> getJson2(url)).thenAccept(this::testShowImg);
+        }
         System.out.println("IIIIIIIIIIIIIIIIIIIII");
 
         layout.setOnTouchListener((v, event) -> {
@@ -76,6 +92,83 @@ public class MainActivity extends AppCompatActivity{
             }
             return true;
         });
+    }
+    public static JSONObject getJson(URL url) {
+        String json = null;
+        try {
+            json = IOUtils.toString(url, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            return new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public JSONObject getJson2(URL url) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.addRequestProperty("username","sample_user");
+            connection.addRequestProperty("password","sample_password");
+            connection.connect();;
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line+"\n");
+                Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+            }
+            return new JSONObject(buffer.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public void testShowImg(JSONObject json) {
+        try {
+            JSONArray jsonArray = json.getJSONArray("images"); // wchodze głebiej, mam tablice 0,1,2
+            String buffer = jsonArray.get(1).toString(); // pobieram element 1, zamieniam na string
+            System.out.println(buffer);
+            JSONObject jsonNested = new JSONObject(buffer); // zamieniam string na json
+            System.out.println(jsonNested.get("base64"));
+            String buffer2 = jsonNested.get("base64").toString();// pobieram element base64 z listy
+            buffer = buffer2.substring(buffer2.indexOf(",") + 1); // usuwam niepotrzebne rzeczy
+            buffer.trim();
+            String encodedImage = buffer;
+            byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap(decodedByte);
+        }catch (JSONException e){}
+
     }
 
     private class JsonTask extends AsyncTask<String, String, String> {
